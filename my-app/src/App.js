5df81a803169a8a79772bb6df6ff86ce682352f3
FormatEfe <142121102+FormatEfe@users.noreply.github.com>
2023-08-13 00:58:40 +0300

    script added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,11 +9,28 @@ function App() {
       <Container>
         <h1>SUDEBALIM SENİ ÇOK SEVİYORUM</h1>
         <div className="ratio ratio-16x9">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/gHh0qjeH2g0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/gHh0qjeH2g0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
+        <p className="paragraf">
+          Aşkım öncelikle senden olan ne varsa hepsi için özür diliyorum çünkü gerçekten şu anki kafam eskide olsa asla hiç birini
+          yapmayıp ilk günkü gibi senle birlikte mutlu bir şekilde vakit geçirip bir hayat yaşardık.
+          Ama ben seni yaptıklarımla çok üzdüm biliyorum gerçekten çok özür dilerim. Ben seni asla hiç bir zaman üzmemeye kendime söz
+          verdim ve bu sözü tutacağım. Hiç bir şekilde senin rahatsız olacağın bir şey yapmayıp her zaman her vakit yanında olup seni
+          mutlu edeceğime hem kendime hem kainata söz veriyorum.
+        </p>
+        <p className="paragraf">
+          Tanıştığımız ilk günden beri gerçekten sana aşığım. Gerçekten mükemmel bir insansın senin gibi bir insan daha hayatımda hiç
+          tanımadım ve gerçekten kalbimde bu kadar büyük bir etki yaratıp yer edinen ilk ve son kişisin. Seni çok seviyorum ilk başından
+          beri sevgilim evet yaptığım hatalar büyük olsa bile hiç birini sana karşı olumsuz duygular beslediğimden değildi. Bir sürü hatalar
+          yaptım ama bunların hepsini düzelteceğim sevgilim.
+        </p>
+        <p className="paragraf">
+          Beni affet...
+        </p>
+        <p className="paragraf-sag">
+          -Efeberk
+        </p>
       </Container>
-      
-      
     </div>
   );
 }
